71e5d236db4d00d8d2d71ffc9398da7768c18c23
pierluigicarrieri <pierluigi.carrieri.90@gmail.com>
2023-09-25 15:52:47 +0200

    Created public layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-</head>
-<body>
+@extends('layouts.public');
 
-    <div class="container">
+@section('content')
 
-    <h1 class="text-danger" >Ciaoooooo</h1>
+<h1>Ciaooooo</h1>
 
-    </div>
-    
-</body>
-</html>
+@endsection
